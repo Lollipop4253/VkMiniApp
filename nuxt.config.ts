680618991ef09434a03db6f4273be2118b4b9fcf
lollipop4253 <mishamx04@gmail.com>
2026-04-20 23:39:49 +0500

    change config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
+  app: {
+    baseURL: 'https://github.com/Lollipop4253/VkMiniApp'
+  },
+  nitro: {
+    preset: 'github-pages'
+  }
 })
